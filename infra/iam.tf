d6a83833f1bbdc52f93d1a98a1c7b30ebadf42a2
amreenshaikh613 <amreen23287@gmail.com>
2026-04-15 21:37:31 -0600

    fix IRSA role_name_prefix issue

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,12 +1,6 @@
 module "irsa" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.39.0"
 
   role_name = "fluent-bit-role"
-
-  oidc_providers = {
-    main = {
-      provider_arn = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["amazon-cloudwatch:fluent-bit"]
-    }
-  }
 }
